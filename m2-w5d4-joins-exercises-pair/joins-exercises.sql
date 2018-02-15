2b7380b7f685bc97fc1718145767a93923dc81d4
benson <mightyrockyman@gmail.com>
2018-02-15 16:43:09 -0500

    pair update yo

--- m2-w5d4-joins-exercises-pair/joins-exercises.sql
+++ m2-w5d4-joins-exercises-pair/joins-exercises.sql
@@ -3,18 +3,35 @@
 
 -- 1. The city name, country name, and city population of all cities in Europe with population greater than 1 million
 -- (36 rows)
+select city.name, country.name, city.population from city
+join country on city.countrycode = country.code
+where continent = 'Europe' and city.population > 1000000;
 
 -- 2. The city name, country name, and city population of all cities in countries where French is an official language and the city population is greater than 1 million
 -- (2 rows)
+select city.name, country.name, city.population from city
+join country on city.countrycode = country.code
+join countrylanguage on city.countrycode = countrylanguage.countrycode
+where language = 'French' and countrylanguage.isofficial = 'true' and city.population > 1000000
+
 
 -- 3. The name of the countries and continents where the language Javanese is spoken
 -- (1 row)
+select country.name, country.continent from country
+join countrylanguage on country.code = countrylanguage.countrycode
+where countrylanguage.language = 'Javanese'
 
 -- 4. The names of all of the countries in Africa that speak French as an official language
 -- (5 row)
+select country.name from country
+join countrylanguage on country.code = countrylanguage.countrycode
+where language = 'French' and countrylanguage.isofficial = 'true' and country.continent = 'Africa'
 
 -- 5. The average city population of cities in Europe
 -- (average city population in Europe: 287,684)
+select avg(city.population) from city
+join country on city.countrycode = country.code
+where country.continent = 'Europe'
 
 -- 6. The average city population of cities in Asia
 -- (average city population in Asia: 395,019)
