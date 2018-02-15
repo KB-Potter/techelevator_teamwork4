9b98c3bc4be08b4c1bfa8960c9a77a08de49da39
Katie <katie.beatrice.potter@gmail.com>
2018-02-15 17:01:19 -0500

    Finished pair day1 exercise

--- m2-w5d4-joins-exercises-pair/joins-exercises.sql
+++ m2-w5d4-joins-exercises-pair/joins-exercises.sql
@@ -36,14 +36,35 @@ where country.continent = 'Europe'
 -- 6. The average city population of cities in Asia
 -- (average city population in Asia: 395,019)
 
+select avg(city.population) from city
+join country on city.countrycode = country.code
+where country.continent = 'Asia'
+
 -- 7. The number of cities in countries where English is an official language
 -- (number of cities where English is official language: 523)
+
+select count(city.name) from city
+join countrylanguage on city.countrycode = countrylanguage.countrycode
+where language = 'English' and countrylanguage.isofficial = 'true'
 
 -- 8. The average population of cities in countries where the official language is English
 -- (average population of cities where English is official language: 285,809)
 
+select avg(city.population) from city
+join countrylanguage on city.countrycode = countrylanguage.countrycode
+where language = 'English' and countrylanguage.isofficial = 'true'
+
 -- 9. The names of all of the continents and the population of the continent’s largest city
 -- (6 rows, largest population for North America: 8,591,309)
 
+select country.continent, MAX(city.population) from country
+join city on city.countrycode = country.code
+GROUP BY country.continent 
+
 -- 10. The names of all of the cities in South America that have a population of more than 1 million people and the official language of each city’s country
 -- (29 rows)
+
+select city.name, countrylanguage.language from city 
+join country on country.code = city.countrycode
+join countrylanguage on city.countrycode = countrylanguage.countrycode
+where city.population > 1000000 and country.continent = 'South America' and countrylanguage.isofficial = 'true'
